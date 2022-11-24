d6a36caa21ad6fd0fc30bc00c8ba962ac74431d1
akshitdayal1 <akshitdayal@gmail.com>
2022-11-24 15:35:35 -0800

    created file

---
+++ secrets.py
@@ -0,0 +1,11 @@
+
+
+secrets = {
+    'ssid' : "WIFI_SSID",
+    'pw' : 'WIFI_PWD',
+    'ac_sid' : "TWILIO_ACCNT_SID",
+    'ac_pwd' : "TWILIO_PWD"
+    }
+
+~                                                                                                                           
+~                
